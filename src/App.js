b2abfa560ebc51156d008c785dc9880eb593522d
sergiomz1 <85529375+sergiomz1@users.noreply.github.com>
2024-03-02 15:54:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routers, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
 import "./App.css";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <div>
-          <TooltipComponent>
-            <button>
-              <FiSettings></FiSettings>
-              <h2>HOLA</h2>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position='Top'>
+            <button
+              type="button"
+              className="text-3xl p- 3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              style={{ background: "blue", borderRadius: "50%" }}
+            >
+              <FiSettings />
             </button>
           </TooltipComponent>
         </div>
       </div>
     </BrowserRouter>
-  );
+  );
 };
